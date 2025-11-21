7e8b48e6d482bf49b777d59769b31d14614bc978
etdan <asfawdaniel779@gmail.com>
2025-11-21 11:56:13 +0300

    Add cmd/main.go

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	_ "github.com/etdan/x-ray/docs"
+	"github.com/etdan/x-ray/initiator"
+)
+
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
+func main() {
+	initiator.Init()
+}
