265e6ed9b990c8d4b7d2ca951b9e44f6f0791241
etdan <asfawdaniel779@gmail.com>
2025-11-22 14:56:24 +0300

    Add cmd/server/server.go

---
+++ cmd/server/server.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type server struct {
+	app *fiber.App
+}
+
+func NewServer(app *fiber.App) server {
+	return server{
+		app: app,
+	}
+}
+
+func (s *server) StartServer(port string) {
+	slog.Info("starting server....")
+	go func() {
+		if err := s.app.Listen(fmt.Sprintf(":%s", port)); err != nil {
+			slog.Error("error server down")
+		}
+		slog.Info("server is up and running")
+	}()
+}
+
+func (s *server) StopServer() {
+	q := make(chan os.Signal, 1)
+	signal.Notify(q, syscall.SIGINT, syscall.SIGTERM)
+	<-q
+	slog.Info("stopping server...")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	if err := s.app.ShutdownWithContext(ctx); err != nil {
+		slog.Info("server stopped by force")
+	} else {
+		slog.Info("server stopped by signal")
+	}
+}
