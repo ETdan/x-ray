d9c9c91446fba8bc9128dccd850ef61bede54539
etdan <asfawdaniel779@gmail.com>
2026-01-02 10:23:50 +0300

    Add internal/glue/interview/route.go

---
+++ internal/glue/interview/route.go
@@ -0,0 +1,56 @@
+package interview_route
+
+import (
+	"net/http"
+
+	port "github.com/etdan/x-ray/internal/constants/interface"
+	"github.com/etdan/x-ray/internal/glue"
+	"github.com/etdan/x-ray/middleware"
+	"github.com/gofiber/fiber/v2"
+)
+
+func InitInterviewRoute(r fiber.Router, handler port.InterviewHandler, authMiddleware middleware.AuthMiddleware) {
+	routes := []glue.Route{
+		{
+			Method:  http.MethodPost,
+			Path:    "/interview",
+			Handler: handler.CreateInterview,
+			// Middlewares: []fiber.Handler{
+			// 	authMiddleware.Authentication,
+			// },
+		},
+		{
+			Method:  http.MethodGet,
+			Path:    "/interview",
+			Handler: handler.GetInterviewsByPagination,
+			// Middlewares: []fiber.Handler{
+			// 	authMiddleware.Authentication,
+			// },
+		},
+		{
+			Method:  http.MethodGet,
+			Path:    "/interview/{id}",
+			Handler: handler.GetInterviewByID,
+			// Middlewares: []fiber.Handler{
+			// 	authMiddleware.Authentication,
+			// },
+		},
+		{
+			Method:  http.MethodGet,
+			Path:    "/interview/company/{id}",
+			Handler: handler.GetInterviewByCompanyID,
+			// Middlewares: []fiber.Handler{
+			// 	authMiddleware.Authentication,
+			// },
+		},
+		{
+			Method:  http.MethodGet,
+			Path:    "/interview/user/{id}",
+			Handler: handler.GetInterviewByUserID,
+			// Middlewares: []fiber.Handler{
+			// 	authMiddleware.Authentication,
+			// },
+		},
+	}
+	glue.RegisterRoutes(r, routes)
+}
