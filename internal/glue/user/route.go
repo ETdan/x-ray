8488df9281b4ef27c7ae24e44c449ccf931f3daf
etdan <asfawdaniel779@gmail.com>
2026-01-03 14:56:52 +0300

    Add internal/glue/user/route.go

---
+++ internal/glue/user/route.go
@@ -0,0 +1,40 @@
+package user_route
+
+import (
+	"net/http"
+
+	port "github.com/etdan/x-ray/internal/constants/interface"
+	"github.com/etdan/x-ray/internal/glue"
+	"github.com/etdan/x-ray/middleware"
+	"github.com/gofiber/fiber/v2"
+)
+
+func InitUserRoute(r fiber.Router, handler port.UserHandler, authMiddleware middleware.AuthMiddleware) {
+	routes := []glue.Route{
+		{
+			Method:  http.MethodGet,
+			Path:    "/user/{id}",
+			Handler: handler.GetUserByID,
+			// Middlewares: []fiber.Handler{
+			// 	authMiddleware.Authentication,
+			// },
+		},
+		{
+			Method:  http.MethodGet,
+			Path:    "/user",
+			Handler: handler.GetUsersByPagination,
+			Middlewares: []fiber.Handler{
+				authMiddleware.Authentication,
+			},
+		},
+		{
+			Method:  http.MethodGet,
+			Path:    "/user/{google-id}",
+			Handler: handler.GetUserByGoogleID,
+			Middlewares: []fiber.Handler{
+				authMiddleware.Authentication,
+			},
+		},
+	}
+	glue.RegisterRoutes(r, routes)
+}
