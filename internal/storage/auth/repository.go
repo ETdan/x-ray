2b92fa104d788f54fecc8a1bf6ef1177c2e704b1
etdan <asfawdaniel779@gmail.com>
2026-01-17 11:37:06 +0300

    Add internal/storage/auth/repository.go

---
+++ internal/storage/auth/repository.go
@@ -0,0 +1,50 @@
+package auth_storage
+
+import (
+	"log/slog"
+	"time"
+
+	"github.com/etdan/x-ray/internal/constants/dto"
+	"github.com/etdan/x-ray/internal/constants/model"
+	"github.com/etdan/x-ray/internal/storage"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type AuthRepository struct {
+	db *gorm.DB
+}
+
+// CreateUser implements storage.UserRepository.
+func (a AuthRepository) CreateUser(gc dto.GoogleIDTokenClaims) (uuid.UUID, error) {
+	uuid := uuid.New()
+	tx := a.db.Create(model.User{
+		ID:        uuid,
+		GoogleID:  gc.Subject,
+		Email:     gc.Email,
+		CreatedAt: time.Now().Unix(),
+		UpdatedAt: time.Now().Unix(),
+		LastLogin: time.Now().Unix(),
+	})
+
+	if tx.Error != nil {
+		slog.Info("error creating user:", tx.Error)
+	}
+	return uuid, tx.Error
+}
+
+// FindUserBySub implements storage.UserRepository.
+func (a AuthRepository) FindUserBySub(sub string) (model.User, error) {
+	var user model.User
+	err := a.db.Where(model.User{GoogleID: sub}).First(&user).Error
+	if err != nil {
+		slog.Info("user lookup for failed:", sub)
+	}
+	return user, err
+}
+
+func NewAuthRepository(db *gorm.DB) storage.OauthRepository {
+	return AuthRepository{
+		db: db,
+	}
+}
